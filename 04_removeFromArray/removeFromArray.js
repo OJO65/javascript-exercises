81aacb2e364326fa09c141103f78be4f1ef3159e
OJO65 <Dmallowa@gmail.com>
2023-07-26 16:31:13 +0300

    7 tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
+// removeFromArray.js
 
-};
+function removeFromArray(array, ...itemsToRemove) {
+    return array.filter(item => !itemsToRemove.includes(item));
+  }
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
