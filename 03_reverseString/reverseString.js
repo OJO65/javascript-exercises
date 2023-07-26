4a123bdb76d8738c2da574090a5eda09f2b62788
OJO65 <Dmallowa@gmail.com>
2023-07-26 13:31:39 +0300

    2 passes 3 fails

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,28 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  // Handle blank strings
+  if (str.trim().length === 0) {
+    return str;
+  }
 
+  // Split the string into words
+  const words = str.split(/\s+/);
+
+  // Reverse each word and join back with spaces
+  const reversedWords = words.map(word => {
+    const isWordWithPunctuation = /\W/.test(word);
+    if (isWordWithPunctuation) {
+      // For words with punctuation, reverse only the word characters
+      const wordCharacters = word.match(/[a-zA-Z0-9]/g);
+      if (wordCharacters) {
+        return wordCharacters.reverse().join('') + word.replace(/[a-zA-Z0-9]/g, '');
+      }
+    }
+    return word.split('').reverse().join('');
+  });
+
+  return reversedWords.join(' ');
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
