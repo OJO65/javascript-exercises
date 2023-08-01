8186d8a17591601cb294df1583a195ca75a01990
OJO65 <Dmallowa@gmail.com>
2023-08-01 16:18:52 +0300

    passed all 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
 
-};
+function isLeapYear(year) {
+  if (year % 4 === 0) {
+    if (year % 100 !== 0 || (year % 100 === 0 && year % 400 === 0)) {
+      return true;
+    }
+  }
+  return false;
+}
 
-// Do not edit below this line
-module.exports = leapYears;
+module.exports = isLeapYear;
