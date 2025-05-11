8116847ccca24c79c94e7bdbad6d4cf0b42d05d3
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-05-11 17:42:50 +0300

    chore: fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const fib = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        fib[i] = fib[i -1] + fib[i - 2];
+    }
+    return fib;
 
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+console.log(fibonacci(2))
+console.log(fibonacci(3))
