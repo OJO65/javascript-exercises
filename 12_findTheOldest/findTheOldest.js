cc693126484d4eafe913e259b906e0325f4e4ed0
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-05-11 17:43:27 +0300

    chore: function to find oldest age

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce((oldest, person) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+    return personAge > oldestAge ? person : oldest;
+  });
+  function getAge(yearOfBirth, yearOfDeath) {
+    if (!yearOfDeath) {
+      yearOfDeath = new Date().getFullYear();
+    }
+    return yearOfDeath - yearOfBirth;
+  }
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
+console.log(findTheOldest([
+  { name: 'Carly', yearOfBirth: 1942, yearOfDeath: 2020 },
+  { name: 'Ray', yearOfBirth: 1965 },
+  { name: 'Jane', yearOfBirth: 1970, yearOfDeath: 2015 }
+]))
