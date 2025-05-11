8881af50e8577e57330b9e2bff4275ac63a3a3d2
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-05-11 17:43:10 +0300

    chore: function to get tiitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  return books.map(book => book.title);
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+console.log(getTheTitles([
+    { title: 'Book 1', author: 'Author 1' },
+    { title: 'Book 2', author: 'Author 2' },
+    { title: 'Book 3', author: 'Author 3' }
+]))
