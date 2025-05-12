c3017df7ea2d1246a33948c48596ad91c5a5922e
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-05-12 20:34:07 +0300

    caeser cipher function

--- archive/archived_caesar/caesar.js
+++ archive/archived_caesar/caesar.js
@@ -1,6 +1,31 @@
-const caesar = function() {
+const caesar = function(string, shift) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const shiftAmount = shift % 26;
+    let result = '';
+
+    for (let i = 0; i < string.length; i++) {
+        const char = string[i];
+        const isUpperCase = char === char.toUpperCase();
+        const lowerChar = char.toLowerCase();
+
+        if (alphabet.includes(lowerChar)) {
+            let newIndex = (alphabet.indexOf(lowerChar) + shiftAmount) % 26;
+            let newChar = alphabet[newIndex];
+
+            if (isUpperCase) {
+                newChar = newChar.toUpperCase();
+            }
+
+            result += newChar;
+        } else {
+            result += char;
+        }
+    }
+
+    return result;
 
 };
 
 // Do not edit below this line
 module.exports = caesar;
+console.log(caesar('Hello, World!', 3));
